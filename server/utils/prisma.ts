15ff47b51c4a81c67dfbb0eb00c732119c11daa1
ridha-arlian <ridhaarlian@proton.me>
2026-07-25 01:49:37 +0700

    fix(prisma): resolve error

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,8 +1,11 @@
-import { PrismaClient } from "../../generated/prisma/client"
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const connectionString = `${process.env.DATABASE_URL}`;
 
-export const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
